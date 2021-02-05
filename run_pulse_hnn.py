b237c3d2a514a54e87238d20e50afcf1aebce98b
Padraig Gleeson <p.gleeson@gmail.com>
2021-02-05 17:48:18 +0000

    More tests

--- run_pulse_hnn.py
+++ run_pulse_hnn.py
@@ -19,11 +19,14 @@ netParams.popParams['L2Pyr']['cellsList'] = [{'x': 100, 'y': 0, 'z': 0}]
 netParams.popParams['L5Basket']['cellsList'] = [{'x': 200, 'y': 0, 'z': 0}]
 netParams.popParams['L5Pyr']['cellsList'] = [{'x': 300, 'y': 0, 'z': 0}]
 netParams.connParams = {}
+'''
+del netParams.popParams['L2Basket']
+'''
 del netParams.popParams['L2Pyr']
 del netParams.popParams['L5Basket']
 del netParams.popParams['L5Pyr']
 
-for pop_id in list(netParams.popParams.keys())[:1]:
+for pop_id in list(netParams.popParams.keys())[:]:
     netParams.stimSourceParams['Input_%s'%pop_id] = {'type': 'IClamp', 'del': 10, 'dur': 80, 'amp': 0.6}
     netParams.stimSourceParams['Input_%s_hyp'%pop_id] = {'type': 'IClamp', 'del': 110, 'dur': 80, 'amp': -0.2}
     netParams.stimTargetParams['Input_%s_targ'%pop_id] = {'source': 'Input_%s'%pop_id, 'sec':'soma', 'loc': 0.5, 'conds': {'pop':pop_id, 'cellList': range(1)}}
@@ -32,8 +35,10 @@ for pop_id in list(netParams.popParams.keys())[:1]:
 simConfig.saveDat = True
 simConfig.analysis = {}
 simConfig.duration = 220
-pp.pprint(simConfig.todict())
-pp.pprint(netParams.todict())
+simConfig.dt = 0.05
+simConfig.saveJson = True
+#pp.pprint(simConfig.todict())
+#pp.pprint(netParams.todict())
 
 from netpyne import sim  # import netpyne sim module
 
